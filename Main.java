43e28debd9668047d859f9234b0a924f6d250cc4
wukaixingxp <wuk@carleton.edu>
2017-02-27 14:42:53 -0600

    With AI

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Kaixing on 2/27/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Board game = new Board();
+        game.startGame();
+//        game.generateRandom();
+//        game.generateRandom();
+//        game.printBoard();
+//        System.out.println(game.isFull());
+//        game.rollDown();
+//        game.printBoard();
+//        System.out.println(game.isFull());
+        game.play();
+
+    }
+}
